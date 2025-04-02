ae54f6e85d98083c91ef2558e46c9647f6337bc0
GuilhermeAviacao <guilherme.aviacao@gmail.com>
2025-04-02 22:00:33 +0200

    adding Posts

--- env.py
+++ env.py
@@ -2,3 +2,5 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_FLtliWBu8zP6@ep-sweet-fire-a28u7qhk.eu-central-1.aws.neon.tech/crust_thigh_write_565269")
+
+os.environ.setdefault("SECRET_KEY", "django-insecure-szsj+1z8in=4&=+_um97g#i&(pn0&rvcujx0p(1nw$ni%(u@#!")
